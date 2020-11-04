61ca627ab57aff7d336542197fdad1c90f61f64f
IMF <ipmafr@gmail.com>
2020-11-04 18:03:40 +0200

    test component now works

--- client/src/MapLeaflet.tsx
+++ client/src/MapLeaflet.tsx
@@ -11,13 +11,14 @@ function MapLeaflet(): JSX.Element {
             style={{ height: '100%', width: '100%' }}
             center={{ lat: 60.47179, lng: 22.23259 }}
             zoom={13}
+            maxBounds={[[81.2, -74.3], [30.9, 121.2]]}
             doubleClickZoom={false}
             zoomControl={false}
             attributionControl={false}>
 
             <TileLayer
-                url="https://cdn.digitransit.fi/map/v1/hsl-map-256/{z}/{x}/{y}.png"
-                maxZoom={18} minZoom={8}
+                url="https://cdn.digitransit.fi/map/v1/hsl-map/{z}/{x}/{y}.png"
+                maxZoom={18} minZoom={5}
                 tileSize={512}
                 zoomOffset={-1}
             />
--- client/src/Test.tsx
+++ client/src/Test.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useMapEvent } from 'react-leaflet';
+import L from 'leaflet';
 
 function Test(): JSX.Element {
     const [zoomLevel, setZoomLevel] = useState(13);
     const [coords, setCoords] = useState('');
 
+    const divRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
-        
-    })
+        if (divRef.current) L.DomEvent.disableClickPropagation(divRef.current);
+    }, []);
 
     const map = useMapEvent('zoom', () => {
         setZoomLevel(map.getZoom());
@@ -18,14 +21,15 @@ function Test(): JSX.Element {
     });
 
     return (
-        <div style={{
-            position: 'absolute',
-            zIndex: 1000,
-            cursor: 'auto',
-            backgroundColor: 'white',
-            fontSize: 'xx-large',
-            width: '100%'
-        }}>
+        <div ref={divRef}
+            style={{
+                position: 'absolute',
+                zIndex: 1000,
+                cursor: 'auto',
+                backgroundColor: 'white',
+                fontSize: 'xx-large',
+                width: '100%'
+            }}>
             {zoomLevel}&nbsp;{coords}
         </div>
     );
