a18d22880bda3494921519f1e0aba7414cb004e4
IMF <ipmafr@gmail.com>
2020-11-06 11:49:14 +0200

    fiddling around

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,10 @@ import './App.css';
 function App(): JSX.Element {
   return (
     <div className='h-screen w-screen bg-white'> {/* They gray here is for debuging */}
-      <MainNav />
-    <MapLeaflet>
-      {/* Custom components on top of the map can go here too */}
-    </MapLeaflet>
+        <MainNav />
+      <MapLeaflet>
+        {/* Custom components on top of the map can go here too */}
+      </MapLeaflet>
     </div>
   );
 }
