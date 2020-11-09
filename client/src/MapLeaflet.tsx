0c4c62da997c749f32d94e21cf9c8d449fc19478
IMF <ipmafr@gmail.com>
2020-11-09 15:37:33 +0200

    cleanup

--- client/src/MapLeaflet.tsx
+++ client/src/MapLeaflet.tsx
@@ -23,13 +23,8 @@ function MapLeaflet(props: IMapProps): JSX.Element {
         right: '0',
         cursor: 'grab'
     });
-    // setGoogleLines will be needed later
     // eslint-disable-next-line
     const [googleLines, setGoogleLines] = useState<string[]>([]);
-    //     'svfnJ_jdwCtAxFVx@zBbGx@vBbCbFxB|DnDfEb@r@P^^n@BBhApCj@`Bv@fDn@hE\\hDRbDTnLDtGJbGXtZA~EI`DMxCY`Gs@|MUrFEpE@jDFrDLbDLrBVnCb@lDr@hExC|Nt@lEr@jFp@~Fv@vHjBvPrBlRfA~JXlCZ`DZtC`@vD\\`EPzCJhCDjC?rCArCKfEOlDWbD]nC[|Bi@zCq@|CcAhDo@lBwCnH}@dCm@tBs@lCo@rCg@lCc@tCm@dFmChWUbCuCxYQ~A_Dd\\E^gHzr@sAdMm@vDo@zC}@dDu@rBy@pBq@lAkAfBgArAsAjAuA~@eAd@aATcAPsAJ{@AgAKmASmAe@mAq@sAaAgEmD_Aw@qAiAqAcAmDsCiDoCkB}AeAw@gAo@cAa@oA[y@G_A@}@H{@Rw@Tw@b@u@h@u@r@o@v@m@z@}@fB{@pBi@~Ae@~A_@fBc@fCYjCUnCMpCK`DIrDEdDEhDEjEApEHrDRlDXhCZnB\\jBd@hBf@`Bj@bBn@nAt@rA|@jAl@n@|@r@bAt@rFvC|F|CjAl@vAz@l@\\l@`@t@n@p@n@x@dA~@xA|@fBl@`Bl@jBl@fCp@`Df@pDt@pJXrCBVLlCf@tHTxEFnDDdD@rDAdFCnZCrNErQ?bDAhFEtOAhE?|B?|J?lGBlDLhDPdDTdCX|Bd@`Dh@hCj@bCz@vChB`GfMta@l@nBdBzFnF`QlBdFrBjE`CrDxSrZ~@tAv@tA~@jBv@hBp@rBl@vBh@vB`@zBb@nC|AtLvBvPj@pEHfA"',
-    //     'q}dnJy|~uCBAG_@SaBAIIFe@b@MJ@D?Dy@t@BZTdB@H@H@H@H?B@D@L@D?BRM',
-    //     'e`enJow~uCNTJj@b@nA@LHf@VQP_A`@SNfALfAdAxHDdAPfAJbANfADf@d@hDHn@L|@Z`Cv@nGtA|JlAnGn@~CPf@oAzAa@^qBjAy@Ri@Dm@?k@OqAk@}A@]JKFaBnAq@h@_@j@c@d@u@~@e@r@cDtFs@zAs@~@~@fFT`BLfALrBJfCLzCn@zTPnKAzNAjAE~HI|DOtGU~Im@AYPo@^iB`@cANk@Dy@Rm@VcAl@s@p@g@l@i@z@_A~A{@~BMb@]pAwAjFOf@_ApBe@v@wCrCu@bAk@bAo@vAc@tAs@tCg@tBsAbF]vCOtAi@nFKt@Kz@Kr@a@dAi@x@uAx@cA^a@D{BBw@?uADE?H|BFjBEjB',
-    // ]);
     const [polylines, setPolylines] = useState<JSX.Element[]>([]);
     const [currentBounds, setCurrentBounds] = useState<L.LatLngBounds>();
 
@@ -40,13 +35,9 @@ function MapLeaflet(props: IMapProps): JSX.Element {
                 return PL.decode(line) as L.LatLngTuple[];
             });
 
-            // const newPolylines = decodedLines.map((line, index) => {
-            //     return <Polyline key={index} positions={line} />;
-            // });
-
             const newPolyline = <Polyline key={decodedLines.length + 10000} positions={decodedLines} />;
 
-            // little sketch, latlng.toBounds()?
+            // little sketch, L.latLngBounds()?
             const polylineBounds = L.polyline(decodedLines).getBounds();
 
             setCurrentBounds(polylineBounds);
@@ -82,9 +73,9 @@ function MapLeaflet(props: IMapProps): JSX.Element {
             {/* Free for non-commercial use: https://carto.com/basemaps/ */}
             <TileLayer
                 url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,&nbsp;
-                            &copy; <a href="https://carto.com/attribution">CARTO</a>'
-                maxZoom={7}
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,
+                            &nbsp;&copy; <a href="https://carto.com/attribution">CARTO</a>'
+                maxZoom={7} minZoom={5}
             />
 
             <ZoomControl position='bottomright' />
