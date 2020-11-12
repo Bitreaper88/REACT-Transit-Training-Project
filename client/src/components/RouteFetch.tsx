4e16726cbcbc02693d0fe5dc83afcb714a0849be
IMF <ipmafr@gmail.com>
2020-11-12 10:12:14 +0200

    now properly fetches data from public transit api

--- client/src/components/RouteFetch.tsx
+++ client/src/components/RouteFetch.tsx
@@ -8,68 +8,59 @@ import moment from 'moment';
 
 export interface ICoordinates {
     lat: number;
-    lng: number;
+    lon: number;
 }
+
 export interface IRouteRequest {
-    startPos?: ICoordinates;
-    endPos?: ICoordinates;
+    from?: ICoordinates;
+    to?: ICoordinates;
     waypoints?: ICoordinates[];
+    date: string;
+    time: string;
 }
 
 function RouteFetch(): JSX.Element {
-    const [req, setReq] = useState<IRouteRequest>();
+    const [req, setReq] = useState<IRouteRequest>({
+        date: moment().format('YYYY-MM-DD'),
+        time: moment().format('h:mm:ss')
+    });
     const [publicRespArray, setPublicRespArray] = useState<Types.IData[]>([]);
     const { setPublicRoute } = useContext(ResponseContext);
 
     useEffect(() => {
-        console.log(req);
-
-        if (!req || !req.startPos || !req.endPos) {
+        if (!req.from || !req.to) {
             return;
         }
 
         if (!req.waypoints || !req.waypoints.length) {
 
-            // Public transit API call
-            const variables = {
-                from: {
-                    lat: req.startPos.lat,
-                    lon: req.startPos.lng
-                },
-                to: {
-                    lat: req.endPos.lat,
-                    lon: req.endPos.lng
-                },
-                date: moment().format('YYYY-MM-DD'),
-                time: moment().format('h:mm:ss')
-            };
-
-            console.log(graphQLRequest);
-
             // calling graphQLAPI
             axios(Constants.URL_API, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    data: JSON.stringify({
-                        query: graphQLRequest,
-                        variables: variables
-                    })
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                data: JSON.stringify({
+                    query: graphQLRequest,
+                    variables: req
                 })
-                // .then((response: { data: { data: Types.IData } }) => {
-                .then((response) => {
-                    console.log(response);
-                    if (response.status !== 200) {
-                        alert('Error fetching public transit route!');
-                        return;
-                    }
+            }).then((response) => {
+                if (response.status !== 200) {
+                    alert('Error fetching public transit route!');
+                    return;
+                }
+                if (response.data.data.plan.itineraries.length === 0){
+                    alert('Could not find a public route from or to your destination.');
+                    return;
+                }
 
-                    const result: Types.IData = response.data.data;
-                    console.log('bar');
-                    setPublicRespArray([result]);
-                })
-                .catch(() => alert('Error connecting to public transit API!'));
+                const result: Types.IData = response.data.data;
+                setPublicRespArray([result]);
+            }).catch((err) => {
+                alert('Error connecting to public transit API!');
+                console.log(err);
+                return;
+            });
         }
     }, [req]);
 
@@ -80,13 +71,14 @@ function RouteFetch(): JSX.Element {
     return (
         // Dev Button
         <button onClick={() => setReq({
-            startPos: {
+            ...req,
+            from: {
                 lat: 60.45169,
-                lng: 22.26686
+                lon: 22.26686
             },
-            endPos: {
+            to: {
                 lat: 61.49774,
-                lng: 23.76129
+                lon: 23.76129
             }
         })}>
             devFetch
