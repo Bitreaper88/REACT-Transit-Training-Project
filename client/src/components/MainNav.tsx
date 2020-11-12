67deb536d3a394a39e2238ce5ecdc57d89f9d74e
Bit_reaper <bitreaper88@gmail.com>
2020-11-12 09:33:37 +0200

    add car form

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -46,13 +46,20 @@ const MainNav: React.FC = () => {
     setCarStatus(!carStatus);
   }
 
+  // function test(){
+  //   alert('test alert');
+  // }   <button onClick={test} className='bg-red-700 absolute'>test</button>
+
   return (
     <div className='grid h-screen w-screen'>
       
       <div className='bg-blue-500 rounded-br-lg h-12 md:z-50 z-0 w-64 absolute top-0 left-0 font-bold p-2 text-white'>
         <img className='object-contain h-8' src='../magenta_logo.png'/>
+        
       </div>
       <div className={sideBarStyle}>
+
+   
         SideBar
         <div className='absolute pt-10 inset-y-0 left-0 h-full transform translate-x-64 flex items-stretch'>
          <button onClick={() => translateSideBar()}
@@ -63,8 +70,9 @@ const MainNav: React.FC = () => {
          </button>
         </div>
 
-        <div className='p-2 bg-white text-gray-800'>
+        <div className='p-2 bg-white text-gray-800 pointer-events-auto'>
           Sidebar components are loaded in here
+          
         </div>
       </div>
 
@@ -101,17 +109,17 @@ const MainNav: React.FC = () => {
       </div>
 
 
-      <div className='absolute md:z-20 z-40 w-full md:mt-12 no pointer-events-none'>
+      <div className='absolute md:z-20 z-40 md:mt-12 pointer-events-none'>{/* pointer-events-none very importatn as this container covers part of the side bar*/}
        <div className={carStyle}>
-          <div className='md:h-58 shadow bg-white w-48 md:ml-4 md:mr-auto md:relative absolute right-0 mr-16 md:rounded-none h-40 rounded-b'>
-            <h1>Car</h1>
+          <div className='md:h-58 shadow bg-white w-64 md:ml-4 md:mr-auto md:relative absolute right-0 mr-16 md:rounded-none h-40 rounded-b pointer-events-auto'>
+              <CarSetup/>
           </div>
         </div>
       </div>
-
-      <div className='md:absolute z-20 md:mt-12 md:bottom-auto w-full fixed bottom-0'>
+      
+      <div className='md:absolute z-20 md:mt-12 md:bottom-auto w-full fixed bottom-0  pointer-events-none'> {/* pointer-events-none very importatn as this container covers part of the side bar*/}
        <div className={comparisonStyle}>
-          <div className='transform xl:translate-x-0 md:translate-x-24 md:h-58 shadow bg-white md:w-1/2 md:mx-auto md:rounded-none h-40 rounded-b'>
+          <div className='transform xl:translate-x-0 md:translate-x-24 md:h-58 shadow bg-white md:w-1/2 md:mx-auto md:rounded-none h-40 rounded-b pointer-events-auto'>
             <h1>Transport comparison</h1>
           </div>
         </div>
@@ -122,5 +130,61 @@ const MainNav: React.FC = () => {
 
   );
 };
+
+
+const CarSetup: React.FC = () => {
+
+  return(
+          <>
+            <div className='p-1 flex items-stretch'>
+              <span className='material-icons overflow-hidden text-blue-500 md-48'>directions_car</span>
+              <span className='text-lg text-gray-700 font-bold mx-1 self-center flex-1 text-left border-b-2 border-blue-500'>
+                Setup your car
+              </span>
+            </div>
+            <form className='w-full mx-1'>
+              <div className='transform translate-x-4'>
+                <div  className='flex items-center pb-2'>
+                    <div className='w-2/5  text-right'>
+                      <label className='block font-medium'>Fuel economy</label>
+                    </div>
+                    <div className='w-1/5 mx-1'>
+                      <input className='bg-gray-200 appearance-none border-b-2 border-blue-500 w-full text-center text-purple-500 font-bold leading-tight focus:outline-none focus:bg-white focus:border-purple-500' step=".1" type='number'/>
+                    </div>
+                    <div className=''>
+                      <label className='block font-medium'>l/100km</label>
+                    </div>
+                </div>
+
+                <div  className='flex items-center pb-2'> 
+                    <div className='w-2/5  text-right'>
+                      <label className='block font-medium'>Fuel tank size</label>
+                    </div>
+                    <div className='w-1/5 mx-1'>
+                      <input className='bg-gray-200 appearance-none border-b-2 border-blue-500 w-full text-center text-purple-500 font-bold leading-tight focus:outline-none focus:bg-white focus:border-purple-500' step="1" type='number'/>
+                    </div>
+                    <div className=''>
+                      <label className='block font-medium'>l</label>
+                    </div>
+                </div>
+                  
+                <div  className='flex items-center pb-2'>
+                    <div className='w-2/5 text-right'>
+                      <label className='block font-medium '>Fuel price</label>
+                    </div>
+                    <div className='w-1/5 mx-1'>
+                      <input className='bg-gray-200 appearance-none border-b-2 border-blue-500 w-full text-center text-purple-500 font-bold leading-tight focus:outline-none focus:bg-white focus:border-purple-500' step=".1" type='number'/>
+                    </div>
+                    <div className=''>
+                      <label className='block font-medium'>€/l</label>
+                    </div>
+                </div>
+              </div>
+            </form>
+          </>
+  );
+};
+
+
 
 export default MainNav;
