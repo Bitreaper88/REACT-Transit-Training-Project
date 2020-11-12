48627828f9bfdafceba1ee5b8047095a52a3c07c
IMF <ipmafr@gmail.com>
2020-11-12 11:04:05 +0200

    add call to car route api & update resp to context

--- client/src/components/RouteFetch.tsx
+++ client/src/components/RouteFetch.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
-import { graphQLRequest } from './RouteFetch.publicAPI';
+import moment from 'moment';
 import * as Constants from '../constants';
 import * as Types from './RouteFetch.types';
+import { graphQLRequest } from './RouteFetch.publicAPI';
+import carAPIcall from './RouteFetch.carAPI';
 import { ResponseContext } from './ResponseContext';
-import moment from 'moment';
 
 export interface ICoordinates {
     lat: number;
@@ -25,48 +26,66 @@ function RouteFetch(): JSX.Element {
         time: moment().format('h:mm:ss')
     });
     const [publicRespArray, setPublicRespArray] = useState<Types.IData[]>([]);
-    const { setPublicRoute } = useContext(ResponseContext);
+    const [carRespArray, setCarRespArray] = useState<Types.ICarRouteAPI[]>([]);
+    const { setPublicRoute, setCarRoute } = useContext(ResponseContext);
 
     useEffect(() => {
-        if (!req.from || !req.to) {
-            return;
-        }
+        if (!req.from || !req.to) return;
 
         if (!req.waypoints || !req.waypoints.length) {
+            (async () => {
 
-            // calling graphQLAPI
-            axios(Constants.URL_API, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                data: JSON.stringify({
-                    query: graphQLRequest,
-                    variables: req
-                })
-            }).then((response) => {
-                if (response.status !== 200) {
-                    alert('Error fetching public transit route!');
+                // calling public transit API
+                axios(Constants.URL_API, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    data: JSON.stringify({
+                        query: graphQLRequest,
+                        variables: req
+                    })
+                }).then((response) => {
+                    if (response.status !== 200) {
+                        alert('Error fetching public transit route!');
+                        return;
+                    }
+                    if (response.data.data.plan.itineraries.length === 0) {
+                        alert('Could not find a public route from or to your destination.');
+                        return;
+                    }
+
+                    const result: Types.IData = response.data.data;
+                    setPublicRespArray([result]);
+                }).catch((err) => {
+                    alert('Error connecting to public transit API!');
+                    console.log(err);
+                    return;
+                });
+
+                // calling car route API
+                const carFrom = `${req.from?.lon},${req.from?.lat}`;
+                const carTo = `${req.to?.lon},${req.to?.lat}`;
+                const carResp = await carAPIcall(`/driving/${carFrom};${carTo}?alternatives=true&geometries=polyline&steps=true&access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`);
+                const carRespData = carResp.data as Types.ICarRouteAPI;
+                
+                if (!carRespData.routes.length) {
+                    alert('Could not find a car route from or to your destination.');
                     return;
                 }
-                if (response.data.data.plan.itineraries.length === 0){
-                    alert('Could not find a public route from or to your destination.');
-                    return;
-                }
 
-                const result: Types.IData = response.data.data;
-                setPublicRespArray([result]);
-            }).catch((err) => {
-                alert('Error connecting to public transit API!');
-                console.log(err);
-                return;
-            });
+                setCarRespArray([carRespData]);
+            })();
         }
     }, [req]);
 
     useEffect(() => {
         setPublicRoute([...publicRespArray]);
     }, [publicRespArray]);
+
+    useEffect(() => {
+        setCarRoute([...carRespArray]);
+    }, [carRespArray]);
 
     return (
         // Dev Button
