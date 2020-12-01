f5643352df29a497520a1abaaca46c719bd81eda
IMF <ipmafr@gmail.com>
2020-12-01 18:01:22 +0200

    now listens to mousemove on document

--- client/src/components/MapMarkerFull.tsx
+++ client/src/components/MapMarkerFull.tsx
@@ -18,7 +18,10 @@ function MapMarkerFull(props: ILMProps): JSX.Element {
 
     const [isBeingDragged, setIsBeingDragged] = useState<boolean>(false);
 
+    /** Rep to the map */
     const mapRef = useRef<L.Map>();
+    /** Ref to the marker being dragged */
+    const divRef = useRef<HTMLDivElement>(null);
 
     // For asserting event types
     function isReactMouseEvent(e: React.MouseEvent<HTMLDivElement, MouseEvent> | React.TouchEvent<HTMLDivElement>):
@@ -38,23 +41,81 @@ function MapMarkerFull(props: ILMProps): JSX.Element {
         return e.type === 'touchstart' || e.type === 'touchmove' || e.type === 'touchend' || e.type === 'touchcancel';
     }
 
-    /** Update marker position while being dragged from UI */
-    function onMouseMove(this: HTMLDivElement, e: MouseEvent) {
-        this.style.top = (e.clientY - 18).toString() + 'px';
-        this.style.left = (e.clientX - 18).toString() + 'px';
+    /** When dragging begins */
+    function onStart(e: React.MouseEvent<HTMLDivElement, MouseEvent> | React.TouchEvent<HTMLDivElement>) {
+        if (isReactMouseEvent(e)) e.preventDefault();
+        setIsBeingDragged(true);
+
+        // Put marker on top
+        e.currentTarget.style.position = 'fixed';
+        e.currentTarget.style.zIndex = '9999';
+
+        // Set styles
+        e.currentTarget.style.cursor = 'grabbing';
+        e.currentTarget.classList.add('md-36');
+
+        if (isReactMouseEvent(e)) {
+
+            // Move marker to mouse position
+            e.currentTarget.style.top = (e.clientY - 18).toString() + 'px';
+            e.currentTarget.style.left = (e.clientX - 18).toString() + 'px';
+
+            // Set up mouse listeners
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseout', onOut);
+            e.currentTarget.addEventListener('mouseup', onStop);
+
+        }
+        else if (isReactTouchEvent(e)) {
+
+            // Move marker to touch position
+            e.currentTarget.style.top = (e.touches[0].clientY - 18).toString() + 'px';
+            e.currentTarget.style.left = (e.touches[0].clientX - 18).toString() + 'px';
+
+            // Set up touch listeners
+            e.currentTarget.addEventListener('touchcancel', onStop);
+            e.currentTarget.addEventListener('touchmove', onTouchMove);
+            e.currentTarget.addEventListener('touchend', onStop);
+        }
+        else console.log('ERROR!');
     }
 
+    /** Update marker position while being dragged */
+    function onMouseMove(this: Document, e: MouseEvent) {
+        if (divRef.current) {
+            divRef.current.style.top = (e.clientY - 18).toString() + 'px';
+            divRef.current.style.left = (e.clientX - 18).toString() + 'px';
+        }
+        else console.log('ERROR!');
+    }
     function onTouchMove(this: HTMLDivElement, e: TouchEvent) {
         this.style.top = (e.touches[0].clientY - 18).toString() + 'px';
         this.style.left = (e.touches[0].clientX - 18).toString() + 'px';
     }
 
-    /** When marker is no longer being dragged */
+    /** Reset marker if mouse is dragged outside current window */
+    function onOut(this: Document) {
+        if (divRef.current) {
+            divRef.current.style.position = '';
+            divRef.current.style.zIndex = '';
+            divRef.current.style.cursor = 'pointer';
+            divRef.current.classList.remove('md-36');
+
+            divRef.current.removeEventListener('mouseup', onStop);
+        }
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseout', onOut);
+        setIsBeingDragged(false);
+    }
+
+    /** When marker is dropped being dragged */
     function onStop(this: HTMLDivElement, e: MouseEvent | TouchEvent) {
 
         let elementUnderMarker;
         let containerPoint;
 
+        // Find final coordinates & element underneath dropped marker
         if (isMouseEvent(e)) {
             this.style.pointerEvents = 'none';
             elementUnderMarker = document.elementFromPoint(e.clientX, e.clientY);
@@ -86,8 +147,8 @@ function MapMarkerFull(props: ILMProps): JSX.Element {
 
         // Clean up afterwards
         if (isMouseEvent(e)) {
-            this.removeEventListener('mousemove', onMouseMove);
-            this.removeEventListener('mouseout', onMouseMove);
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseout', onOut);
             this.removeEventListener('mouseup', onStop);
             setIsBeingDragged(false);
         }
@@ -96,45 +157,6 @@ function MapMarkerFull(props: ILMProps): JSX.Element {
             this.removeEventListener('touchmove', onTouchMove);
             this.removeEventListener('touchend', onStop);
             setIsBeingDragged(false);
-        }
-        else console.log('ERROR!');
-    }
-
-    /** When dragging begins */
-    function onStart(e: React.MouseEvent<HTMLDivElement, MouseEvent> | React.TouchEvent<HTMLDivElement>) {
-        if (isReactMouseEvent(e)) e.preventDefault();
-        setIsBeingDragged(true);
-
-        // Put marker on top
-        e.currentTarget.style.position = 'fixed';
-        e.currentTarget.style.zIndex = '9999';
-
-        // Set styles
-        e.currentTarget.style.cursor = 'grabbing';
-        e.currentTarget.classList.add('md-36');
-
-        if (isReactMouseEvent(e)) {
-
-            // Move marker to mouse position
-            e.currentTarget.style.top = (e.clientY - 18).toString() + 'px';
-            e.currentTarget.style.left = (e.clientX - 18).toString() + 'px';
-
-            // Set up mouse listeners
-            e.currentTarget.addEventListener('mousemove', onMouseMove);
-            e.currentTarget.addEventListener('mouseout', onMouseMove);
-            e.currentTarget.addEventListener('mouseup', onStop);
-
-        }
-        else if (isReactTouchEvent(e)) {
-
-            // Move marker to touch position
-            e.currentTarget.style.top = (e.touches[0].clientY - 18).toString() + 'px';
-            e.currentTarget.style.left = (e.touches[0].clientX - 18).toString() + 'px';
-
-            // Set up touch listeners
-            e.currentTarget.addEventListener('touchcancel', onStop);
-            e.currentTarget.addEventListener('touchmove', onTouchMove);
-            e.currentTarget.addEventListener('touchend', onStop);
         }
         else console.log('ERROR!');
     }
@@ -154,6 +176,7 @@ function MapMarkerFull(props: ILMProps): JSX.Element {
 
                         return (
                             <div key={props.id} className='material-icons select-none theMarker'
+                                ref={divRef}
                                 onMouseDownCapture={(e) => onStart(e)}
                                 onTouchStartCapture={(e) => onStart(e)}
                                 style={{ color: props.color, cursor: 'pointer' }}>
