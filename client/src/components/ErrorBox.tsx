07a4f0c9ac76525d7ad68173bb9e336bc7216810
IMF <ipmafr@gmail.com>
2020-12-01 10:31:02 +0200

    now with shadows

--- client/src/components/ErrorBox.tsx
+++ client/src/components/ErrorBox.tsx
@@ -9,7 +9,7 @@ function ErrorBox(props: IError): JSX.Element | null {
 
     return (
         props.msg ?
-            <div className='fixed top-0 left-0 w-64 p-2 bg-yellow-400 text-base text-center align-middle pointer-events-auto'
+            <div className='fixed top-0 left-0 w-64 p-2 shadow bg-yellow-400 text-base text-center align-middle pointer-events-auto'
                 style={{ zIndex: 9999 }}>
                 <div>{props.msg}</div>
                 <button className='text-sm hover:underline text-red-700 p-3'
