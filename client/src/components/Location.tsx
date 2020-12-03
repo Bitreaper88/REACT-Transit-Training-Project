c58d0ed21fe2b70e445ccc9f09708fa8e21f0b84
IMF <ipmafr@gmail.com>
2020-12-03 18:04:38 +0200

    cleaned up the code

--- client/src/components/Location.tsx
+++ client/src/components/Location.tsx
@@ -2,6 +2,18 @@ import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { ICoordinates } from './RouteFetch';
 import MapMarkerFull from './MapMarkerFull';
+
+/** NOTE: Actual response includes other values as well but only these are used by the App */
+interface IResponse {
+  geometry:
+  {
+    coordinates: [number, number]
+  },
+  properties:
+  {
+    label: string
+  }
+}
 interface IAddress {
   label: string;
   coordinates: [number, number];
@@ -14,47 +26,43 @@ interface IProps {
   };
 }
 
-// const data: IAddress = { label: 'Hello', coordinates: [23.23, 234.4] };
-// console.log(data);
-
 const Location = (props: IProps): JSX.Element => {
   const [search, setSearch] = useState<string>('');
-  // eslint-disable-next-line
-  const [address, setAddress] = useState<string>('');
   const [options, setOptions] = useState<IAddress[]>([]);
   const [display, setDisplay] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
+  // When user input changes
   useEffect(() => {
     if (!search) {
       setOptions([]);
       return;
     }
 
-    const location: IAddress[] = [];
-    async function getLocation() {
+    // Autocomplete API call
+    (async () => {
       const response = await axios.get(
         `https://api.digitransit.fi/geocoding/v1/autocomplete?text=${search}&layers=localadmin,address`
       );
-      const body = await response.data.features;
-      // eslint-disable-next-line
-      body.map((a: any) => {
-        const c: IAddress = {
+      if (response.status !== 200) return;
+
+      const body: IResponse[] = response.data.features;
+      const location = body.map((a) => {
+        return ({
           label: a.properties.label,
-          coordinates: a.geometry.coordinates as [number, number],
-        };
-        return location.push(c);
+          coordinates: a.geometry.coordinates,
+        });
       });
+
       setOptions(location);
-    }
-    getLocation();
+    })();
+
   }, [search]);
 
+  /** When an autocompleted location name is selected */
   const setAddressLabel = (label: string, coordinates: [number, number]) => {
-    setAddress(label);
     setSearch(label);
     props.coordinates.setPosition({ lat: coordinates[1], lon: coordinates[0] });
-    // setCoordinates(coordinates);
     setDisplay(false);
   };
 
@@ -66,19 +74,17 @@ const Location = (props: IProps): JSX.Element => {
     const lat = pos.lat;
     const lon = pos.lng;
 
-    // Find nearby locations and make them selectable in options list
-    let nearbyAddresses: IAddress[] = [];
-
+    // Find nearby locations and make them selectable on options list
     (async () => {
       const resp = await axios.get(`http://api.digitransit.fi/geocoding/v1/reverse?point.lat=${lat}&point.lon=${lon}&size=5&layers=address`);
       if (resp.status === 200 && resp.data.features.length) {
-        const body = resp.data.features as { geometry: { coordinates: [number, number] }, properties: { label: string } }[];
+        const body = resp.data.features as IResponse[];
 
-        nearbyAddresses = body.map((address) => {
+        const nearbyAddresses = body.map((address) => {
           return {
             coordinates: address.geometry.coordinates,
             label: address.properties.label
-          };
+          } as IAddress;
         });
 
         setOptions(nearbyAddresses);
@@ -108,7 +114,7 @@ const Location = (props: IProps): JSX.Element => {
       ></input>
 
       <MapMarkerFull key={props.fieldName} id={props.fieldName}
-        color={props.fieldName === 'Origin' ? '#e53e3e' : '#38a169'} // These color values must be kept in sync with rest of the APP
+        color={props.fieldName === 'Origin' ? '#e53e3e' : '#38a169'} // These color values must be kept in sync with rest of the App
         position={props.coordinates.position ? {
           lat: props.coordinates.position.lat,
           lng: props.coordinates.position.lon
@@ -117,28 +123,23 @@ const Location = (props: IProps): JSX.Element => {
 
       {display && (
         <div className='absolute z-10 left-0 mt-1 py-1 rounded-sm bg-white select-none'>
-          {options
-            // .filter(
-            //   ({ label }) =>
-            //     label.toLowerCase().indexOf(search.toLowerCase()) > -1
-            // )
-            .map((v, i) => {
-              return (
-                <div
-                  onMouseDownCapture={() => setAddressLabel(v.label, v.coordinates)}
-                  tabIndex={0}
+          {options.map((v, i) => {
+            return (
+              <div
+                onMouseDownCapture={() => setAddressLabel(v.label, v.coordinates)}
+                tabIndex={0}
+                key={i}
+                className='bg-white rounded-lg p-1 w-65'
+              >
+                <span
+                  className='bg-white block border-white px-2 py-1 hover:border-white hover:bg-indigo-500 hover:text-white'
                   key={i}
-                  className='bg-white rounded-lg p-1 w-65'
                 >
-                  <span
-                    className='bg-white block border-white px-2 py-1 hover:border-white hover:bg-indigo-500 hover:text-white'
-                    key={i}
-                  >
-                    {v.label}
-                  </span>
-                </div>
-              );
-            })}
+                  {v.label}
+                </span>
+              </div>
+            );
+          })}
         </div>
       )}
     </form>
