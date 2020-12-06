61f198ce5bcb021d2c28190c935c2e0588fc7fa5
Bit_reaper <bitreaper88@gmail.com>
2020-12-06 22:56:31 +0200

    selector

--- client/src/components/AllRoutesWrapper.tsx
+++ client/src/components/AllRoutesWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Leg from './LegRoutes/Leg';
 import StartEnd from './Duration/StartEnd';
 import DateIconLoc from './Duration/DateIconLoc';
@@ -24,29 +24,92 @@ const options = {
   hour12: false,
 };
 
-const RoutesWrapper: React.FC = () => {
+const RoutesSelector: React.FC = () => {
 
-  const { current } = useContext(ResponseContext);
+  const { parsed } = useContext(ResponseContext);
+  const routes: JSX.Element[] = [];
 
-  if (current?.pubDf?.legs){
-    const startTime = new Date(current.pubDf.legs[0].startTime);
-    const endTime = new Date(current.pubDf.legs[current.pubDf.legs.length-1].endTime);
+  parsed?.pubItins.forEach((itin, index) =>{
+    routes.push(<RoutesWrapper key={index+'Routes'} index={index} />);
+  });
+
+  return (
+    <>
+      {routes}      
+    </>
+  );
+
+};
+
+interface IRoutesWrapper {
+  index: number
+ // legs: ILeg[]
+}
+
+const RoutesWrapper: React.FC<IRoutesWrapper>  = (props: IRoutesWrapper ) => {
+   // Currently selected routes, updates any time a new itinerary is selected.
+  
+  const { currentPubItin, parsed, setCurrentPubItin} = useContext(ResponseContext);
+ 
+  if (!parsed) return (<></>);
+  const currentLegs = parsed.pubItins[props.index].legs;
+  let showRouteDef = true;
+  let showRouteCSSDef = 'h-0 ';
+  let selectedDef = ' bg-blue-500 ';
+  if (props.index <= 0){
+    showRouteDef = false;
+    showRouteCSSDef = 'h-auto';
+    selectedDef = ' bg-purple-500 ';
+  }
+  const [showRoute, setShowRoute] = useState(showRouteDef);
+  const [showRouteCSS, setShowRouteCSS] = useState(showRouteCSSDef);
+
+  
+  const [selected, setSelected] = useState(selectedDef);
+
+  function routeToggle() {    
+    if(showRoute) setShowRouteCSS(' h-auto ');
+    else setShowRouteCSS(' h-0 ');
+
+    setShowRoute(!showRoute);
+    setCurrentPubItin(props.index);
+  }
+
+  useEffect(() => {
+    if (currentPubItin === props.index) setSelected(' bg-purple-500 ');
+    else  setSelected(' bg-blue-500 '); 
+  });
+
+
+  if (currentLegs){
+    const startTime = new Date(currentLegs[0].startTime);
+    const endTime = new Date(currentLegs[currentLegs.length-1].endTime);
 
     const legsArray: JSX.Element[] = [];
-    current.pubDf.legs.forEach(legs => {
+    currentLegs.forEach(legs => {
       let agency = '';
       if (legs.agency?.name) agency = legs.agency?.name;
        const startTime = new Date(legs.startTime);
        legsArray.push(<DateIconLoc key={legs.startTime} time={startTime.toLocaleTimeString('en-US', options).toString()} place={legs.from.name} />);
        legsArray.push(<Leg key={legs.endTime-1} time={startTime.toLocaleTimeString('en-US', options).toString()} agency={agency} distance={legs.distance / 1000} mode={legs.mode}/>);
     });
+  
 
     return (
-      <>    
-        <StartEnd key='from' time={startTime.toLocaleTimeString('en-US', options).toString()} place={current.pubDf.legs[0].from.name} />
-        {legsArray}
-        <StartEnd key='to' time={endTime.toLocaleTimeString('en-US', options).toString()} place={current.pubDf.legs[current.pubDf.legs.length-1].from.name} />
-      </>
+      <div className='border-b-2 border-gray-800 mb-2'>
+        <div onClick={routeToggle} className= {selected + 'flex w-full mt-1 transition duration-500 ease-in-out h-6 hover:bg-purple-700'}>
+        {selected === ' bg-purple-500 ' ?  
+          <div className='m-auto text-white font-bold transition duration-500 ease-in-out'>Selected</div>
+        : <></>} 
+        </div> 
+        
+        <div className={showRouteCSS + 'overflow-hidden transition duration-500 ease-in-out'}> 
+         
+          <StartEnd key='from' time={startTime.toLocaleTimeString('en-US', options).toString()} place={currentLegs[0].from.name} />
+          {legsArray}
+          <StartEnd key='to' time={endTime.toLocaleTimeString('en-US', options).toString()} place={currentLegs[currentLegs.length-1].from.name} />
+        </div>
+      </div>
     );
   }else {
     return (
@@ -57,4 +120,4 @@ const RoutesWrapper: React.FC = () => {
 
 };
 
-export default RoutesWrapper;
+export default RoutesSelector;
